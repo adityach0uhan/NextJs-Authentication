0572c55e48926d574352ba9b3669413313bc90c1
adityach0uhan <m4with6x@gmail.com>
2024-04-04 23:09:45 +0530

    Created Database Connection

---
+++ src/dbConfig/db.ts
@@ -0,0 +1,26 @@
+
+import { connect } from "http2";
+import mongoose from "mongoose";
+
+export async function createDatabaseConnection() {
+    
+
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+        connection.on('connect', () => {
+            console.log('Connected to MongoDB');
+        });
+        connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+            process.exit();
+        });
+        
+        
+    } catch (error) {
+        console.log("Error Occurred During Database Connection:", error);
+    }
+
+
+
+}
