b59badc9cec222fd5135fc2dd7c3456c66779820
adityach0uhan <m4with6x@gmail.com>
2024-04-05 10:52:16 +0530

    Added NodeMailer

--- src/helper/mailer.helper.js
+++ src/helper/mailer.helper.js
@@ -1,21 +1,27 @@
 import nodemailer from 'nodemailer'
 
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: process.env.SMTP_USERID,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
 export const sendMail = async ({ email, username, emailType }) => {
-    
-
     try {
-        
-        const transporter = nodemailer.createTransport({
-            host: SMTP_HOST,
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-                user:SMTP_USERID,
-                pass:SMTP_PASSWORD,
-            },
+        const info = await transporter.sendMail({
+            from: "admin",
+            to: email,
+            subject: emailType,
+            text: username,
+            html: "<b>Hello world?</b>",
         });
-
-    } catch (error) {
+        console.log("Message sent: %s", info.messageId);
+    }
+    catch (error) {
         console.log("Error occurred while sending mail ");
     }
     
