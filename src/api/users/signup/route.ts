180cfab7ee22fd3fd54e5a1102f5af8f865456c0
adityach0uhan <m4with6x@gmail.com>
2024-04-05 13:37:53 +0530

    Added Hashing and salting

--- src/api/users/signup/route.ts
+++ src/api/users/signup/route.ts
@@ -1,27 +1,38 @@
 import createDatabaseConnection from '@/dbConfig/db';
 import userModel from '@/model/userModel'
 import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
 
 createDatabaseConnection();
 
 export async function POST(request: NextRequest) {
-  
-    try {
-      
-      const requestBody = request.json
-      const { username, password, email } = requestBody;
-       
-      await userModel.findOne({ email })
-      if (userModel) {
-        return NextResponse.json({ error: "Already Exist" });
-      }
 
+  try {
 
-    } catch (error:any) {
-      
-      return NextResponse.json({error:error.message})
+    const requestBody = request.json
+    const { username, password, email } = requestBody;
 
+    await userModel.findOne({ email })
+    if (userModel) {
+      return NextResponse.json({ error: "Already Exist" });
     }
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const UserData = await new userModel({
+      username,
+      password: hashedPassword,
+      emailid: email,
+    })
+      UserData.save();
+    
+
+
+  } catch (error: any) {
+
+    return NextResponse.json({ error: error.message })
+
+  }
 
 }
 
