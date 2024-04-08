50dcf2e8c5f14392628893370c9d8052ca92e838
adityach0uhan <m4with6x@gmail.com>
2024-04-08 23:22:18 +0530

    Fixed Emailer

--- src/helper/mailer.helper.js
+++ src/helper/mailer.helper.js
@@ -7,7 +7,7 @@ export default async function sendEmail({ email, userID, emailType }) {
     try {
         if (emailType === 'VERIFY') {
 
-            const hashedToken = await bcryptjs.hash(String(userID), 10);
+            const hashedToken = await bcryptjs.hash(userID.toString(), 10);
             userModel.findByIdAndUpdate(userID, {
                 verifyUserToken: hashedToken,
                 verifyUserTokenExpiry: Date.now() + 3600000,
@@ -32,6 +32,15 @@ export default async function sendEmail({ email, userID, emailType }) {
             },
         });
 
+        console.log("All data regarding SMTP",
+            "USerID :",process.env.SMTP_USERID,
+            "user password :", process.env.SMTP_PASSWORD,
+            "smtp port:", process.env.SMTP_PORT,
+            "smtp host :", process.env.SMTP_HOST,
+            "destination email", email
+        )
+
+        const verificationLink = 'http://localhost:3000';
         const info = await transporter.sendMail({
             from: "adityach0uhan@gmail.com",
             to: email,
