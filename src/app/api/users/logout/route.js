3b2d78caf611e4404758e3c0f940b361c2e9fb58
adityach0uhan <m4with6x@gmail.com>
2024-04-11 23:20:48 +0530

    Logout Functionality Done

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -1,6 +1,27 @@
 import createDatabaseConnection from '@/dbConfig/db';
-import userModel from '@/model/userModel'
 import { NextRequest, NextResponse } from 'next/server';
-import bcryptjs from 'bcryptjs';
-import jwt from "jsonwebtoken"
 createDatabaseConnection();
+
+export async function GET(request) {
+    try {
+        
+
+        const response = NextResponse.json({
+            message: "Logout Successful",
+            success:true,
+        })
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0),
+        });
+
+        return response;
+
+
+    } catch (error) {
+        NextResponse.json({
+            message: "Error Occurred in Logout Route",
+            error:error,
+        })
+    }
+}
